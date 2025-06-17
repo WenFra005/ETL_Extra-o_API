8fdbda777ef61b6f771478321950eaf04df670d4
WenFra005 <wendellfrancisco2005@hotmail.com>
2025-06-17 16:36:05 -0300

    FEAT: adiciona modelo DolarData para armazenamento de dados de câmbio

---
+++ src/SQL/database.py
@@ -0,0 +1,16 @@
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String, Float, DateTime
+from datetime import datetime, UTC
+
+Base = declarative_base()
+
+class DolarData(Base):
+    __tablename__ = 'dolar_data'
+    
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    moeda = Column(String(3), nullable=False)
+    moeda_destino = Column(String(3), nullable=False)
+    valor_de_compra = Column(Float, nullable=False)
+    timestamp_moeda = Column(DateTime, default=lambda: datetime.now(UTC), nullable=False)
+    timestamp_criacao = Column(DateTime, default=lambda: datetime.now(UTC), nullable=False)
+    
