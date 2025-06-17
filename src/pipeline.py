76e7e6689a792eee7ab8517aa63e864bccc0eb43
WenFra005 <wendellfrancisco2005@hotmail.com>
2025-06-17 11:50:14 -0300

    FEAT: adiciona função de transformação de dados para formatar a resposta da API

--- src/pipeline.py
+++ src/pipeline.py
@@ -4,7 +4,22 @@ def extract_data():
     url = "https://economia.awesomeapi.com.br/json/last/USD-BRL"
     response = requests.get(url)
     data = response.json()
-
     return data
 
-print(extract_data()["USDBRL"])
+def transform_data(data):
+    moeda = data['USDBRL']['code']
+    valor_de_compra = data['USDBRL']['bid']
+    data_timestamp = data['USDBRL']['timestamp']
+
+    data_transformed = {
+        "moeda": moeda,
+        "valor_de_compra": valor_de_compra,
+        "data_timestamp": data_timestamp
+    }
+
+    return data_transformed
+
+if __name__ == "__main__":
+    data = extract_data()
+    transformed_data = transform_data(data)
+    print(transformed_data)
