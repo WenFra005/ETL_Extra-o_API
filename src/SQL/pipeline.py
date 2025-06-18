a9f51da5ac431d8d3322eef27669a67d448b04eb
WenFra005 <wendellfrancisco2005@hotmail.com>
2025-06-18 11:56:35 -0300

    FEAT: implementa pipeline de dados para extração e armazenamento de informações de câmbio em banco de dados

---
+++ src/SQL/pipeline.py
@@ -0,0 +1,80 @@
+import time
+from turtle import dot
+from dotenv import load_dotenv
+import requests
+from datetime import UTC, datetime
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import os
+from database import Base, DolarData
+
+load_dotenv(dotenv_path=".src\\SQL\\.env")
+
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+
+DATABASE_URL = (
+    f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}"
+    f"@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+)
+
+engine = create_engine(DATABASE_URL)
+Session = sessionmaker(bind=engine)
+
+def create_tables():
+    Base.metadata.create_all(engine)
+    print("Tabelas criadas com sucesso.")
+
+def extract_data():
+    url = "https://economia.awesomeapi.com.br/json/last/USD-BRL"
+    response = requests.get(url)
+    data = response.json()
+    
+    return data
+
+def transform_data(data):
+    moeda_origem = data['USDBRL']['code']
+    moeda_destino = data['USDBRL']['codein']
+    valor_de_compra = data['USDBRL']['bid']
+    timestamp_moeda = datetime.fromtimestamp(int(data['USDBRL']['timestamp']), UTC)
+    timestamp_criacao = datetime.now(UTC)
+
+    data_transformed = {
+        "moeda": moeda_origem,
+        "moeda_destino": moeda_destino,
+        "valor_de_compra": valor_de_compra,
+        "timestamp_moeda": timestamp_moeda,
+        "timestamp_criacao": timestamp_criacao
+    }
+
+    return data_transformed
+
+def save_data_postgres(data):
+    session = Session()
+    novo_registro = DolarData(**data)
+    session.add(novo_registro)
+    session.commit()
+    session.close()
+    print("Dados salvos com sucesso no banco de dados PostgreSQL.")
+
+if __name__ == "__main__":
+    create_tables()
+    print("Iniciando o pipeline de dados...")
+
+    while True:
+        try:
+            data_json = extract_data()
+            if data_json:
+                transformed_data = transform_data(data_json)
+                print("Dados transformados:", transformed_data)
+                save_data_postgres(transformed_data)
+            time.sleep(100)
+        except KeyboardInterrupt:
+            print("Pipeline interrompido pelo usuário.")
+            break
+        except Exception as e:
+            print(f"Ocorreu um erro: {e}")
+            time.sleep(50)
