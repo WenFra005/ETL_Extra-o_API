c12def18564f0bb5e5639e3632982673cd7d4f80
WenFra005 <wendellfrancisco2005@hotmail.com>
2025-06-19 19:59:12 -0300

    FIX: corrige a chamada da função de instrumentação de requisições do logfire

--- src/SQL/pipeline.py
+++ src/SQL/pipeline.py
@@ -16,7 +16,7 @@ logfire.configure()
 basicConfig(handlers=[logfire.LogfireLoggingHandler()])
 logger = getLogger(__name__)
 logger.setLevel(logging.INFO)
-logfire.instrument_requests
+logfire.instrument_requests()
 logfire.instrument_sqlalchemy()
 
 load_dotenv()
