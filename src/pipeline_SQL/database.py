b35c2e5feb69c17823ec0cef98a8db12c1147c5c
WenFra005 <wendellfrancisco2005@hotmail.com>
2025-06-20 17:59:03 -0300

    REFACTOR: renomeia pastas "noSQL" e "SQL" para "pipeline_noSQL" e "pipeline_SQL", respectivamente

--- src/pipeline_SQL/database.py
+++ src/pipeline_SQL/database.py
@@ -4,13 +4,17 @@ from datetime import datetime, UTC
 
 Base = declarative_base()
 
+
 class DolarData(Base):
-    __tablename__ = 'dolar_data'
-    
+    __tablename__ = "dolar_data"
+
     id = Column(Integer, primary_key=True, autoincrement=True)
     moeda_origem = Column(String(3), nullable=False)
     moeda_destino = Column(String(3), nullable=False)
     valor_de_compra = Column(Float, nullable=False)
-    timestamp_moeda = Column(DateTime, default=lambda: datetime.now(UTC), nullable=False)
-    timestamp_criacao = Column(DateTime, default=lambda: datetime.now(UTC), nullable=False)
-    
+    timestamp_moeda = Column(
+        DateTime, default=lambda: datetime.now(UTC), nullable=False
+    )
+    timestamp_criacao = Column(
+        DateTime, default=lambda: datetime.now(UTC), nullable=False
+    )
