eb88d62963ed8bbf1a702120a97222dd3bfa26fb
WenFra005 <wendellfrancisco2005@hotmail.com>
2025-07-08 17:42:36 -0300

    FEAT: adiciona suporte para execução do Flask com Waitress e define host e porta para o serviço

--- src/health_api.py
+++ src/health_api.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from waitress import serve
 
 app = Flask(__name__)
 
@@ -14,3 +15,7 @@ def health():
         Uma mensagem indicando que o serviço está ativo.
     """
     return "Serviço ativo e funcionando!"
+
+
+if __name__ == "__main__":
+    serve(app, host="0.0.0.0", port=10000)
